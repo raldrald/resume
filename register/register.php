195ba7021648f828de63951792d89d955873549b
Jerald Islan <islanjerald06@gmail.com>
2024-09-01 10:29:17 +0800

    create the form for register page

---
+++ register/register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Register</title>
+    </head>
+    <body>
+
+            <div class="container">
+                <form action="" method="post" autocomplete="off" enctype="multipart/form-data">
+                    <div class="signup">
+                        <label for="email"> Email: </label>
+                        <input type="email" name="email" id="email" placeholder="Email"><br>
+
+                        <label for="email"> Password: </label>
+                        <input type="password" name="password" id="password" placeholder="Password"><br>
+
+                        <label for="email"> Confirm Password: </label>
+                        <input type="password" name="confirmpassword" id="confirmpasswor" placeholder="Confirm Password"><br>
+                    </div>
+                    <div class="button">
+                        <button type="submit">Register</button>
+                        <button ><a href="login.php">Go Back</a></button>
+                    </div>
+                    
+                </form>
+            </div>
+
+    </body>
+</html>
