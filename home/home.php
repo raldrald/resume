7cc943e4f550701428d20f9ecbec810fdb802198
Jerald Islan <islanjerald06@gmail.com>
2024-09-03 12:32:35 +0800

    fixing and designing the home page

--- home/home.php
+++ home/home.php
@@ -9,6 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="../home/homestyle.css">
     <title>Document</title>
 </head>
@@ -16,25 +17,42 @@
         <div class="navbar">
           <?php include "../index.php";?>
         </div>
+        <div class="container">
+        
             <div class="profilepic">
+            <p>Profile</p>
 
+            </div>  
+        <div class="info">
+                <p>name</p>
+                <p>course</p>
+                <p>address</p>
+                <p>email</p>
+                <p>number</p>
+       
             </div>
 
-            <div class="info">
-
-            </div>
             <div class="objective">
+                <p>objective</p>
 
             </div>
             <div class="skill">
+                <p>skill</p>
 
             </div>
             <div class="personal-info">
-
+                <p>age</p>
+                <p>birthday</p>
+                <p>place of birth</p>
+                <p>gender</p>
             </div>
             <div class="school">
+                <p>elem</p>
+                <p>jhs</p>
+                <p>shs</p>
+                <p>college</p>
 
             </div>
-
+            </div>
 </body>
 </html>
