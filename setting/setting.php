f9644f1f10dd66b7ad9b5bcdce78c7b3a6722d2f
Jerald Islan <islanjerald06@gmail.com>
2024-09-03 22:50:24 +0800

    continue tommorow

---
+++ setting/setting.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../setting/setting.php">
+    <title>Account Edit</title>
+</head>
+<body>
+       <button><a href="edit-profile.php">Edit Profile</a></button>
+       <button><a href="update.php">Account Update</a></button>
+       <button><a href="logout.php">Logout</a></button>    
+</body>
+</html>
